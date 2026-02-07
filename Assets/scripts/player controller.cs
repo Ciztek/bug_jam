b28e3e70d3cee88021f6e95e56b5e8f9e5fd5c86
mael <mael.gouyet@epitech.eu>
2026-02-07 10:15:15 +0100

    ADD camera movement

--- Assets/scripts/player controller.cs
+++ Assets/scripts/player controller.cs
@@ -2,29 +2,37 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
-
     private Rigidbody rb; 
     private float movementX;
     private float movementY;
+    private Camera mainCamera;
+    private bool moved;
+    
     void Start()
     {
-      rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
     }
 
     void OnMove(InputValue value)
     {
-        Vector2 movementVector = value.Get<Vector2>();; 
+        Vector2 movementVector = value.Get<Vector2>();
         movementX = movementVector.x; 
-        movementY = movementVector.y; 
-        
+        movementY = movementVector.y;
     }
 
     void FixedUpdate()
     {
-        
         transform.Translate(movementX * Time.deltaTime * 4f, 0, movementY * Time.deltaTime * 4f);
+        
+        // Camera follows player
+        if (mainCamera != null)
+        {
+            mainCamera.transform.position = transform.position + new Vector3(-3, 3, 1);
+            mainCamera.transform.LookAt(transform);
+            //  mainCamera.transform.rotation = Quaternion.Euler(15, 5, 0);
+        } else if (mainCamera == null){
+            Debug.LogWarning("Main Camera not found!");
+        }
     }
-
-    
-    
 }
